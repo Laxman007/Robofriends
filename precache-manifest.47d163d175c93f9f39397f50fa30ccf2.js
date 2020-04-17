aa5f3c04e978f94be865799a327dcb058846ea09
laxman <laxmangoel209@gmail.com>
2020-04-17 11:05:25 +0530

    Updates

---
+++ precache-manifest.47d163d175c93f9f39397f50fa30ccf2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e5add2e25077d1213999287d23da5f7",
+    "url": "/Robofriends/index.html"
+  },
+  {
+    "revision": "01dda8862d92b797dfaf",
+    "url": "/Robofriends/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "1858c457944f1f2ae0a5",
+    "url": "/Robofriends/static/css/main.90cd5739.chunk.css"
+  },
+  {
+    "revision": "01dda8862d92b797dfaf",
+    "url": "/Robofriends/static/js/2.a10038f9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Robofriends/static/js/2.a10038f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1858c457944f1f2ae0a5",
+    "url": "/Robofriends/static/js/main.6901940e.chunk.js"
+  },
+  {
+    "revision": "d53e560d43c7171e8016",
+    "url": "/Robofriends/static/js/runtime-main.e0286ea6.js"
+  }
+]);
